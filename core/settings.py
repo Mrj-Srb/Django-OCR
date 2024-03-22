f39783583c284318c6909cf5b18e1a4c15d184d5
Meraj Sarab <merajpython@gmail.com>
2024-03-22 20:43:39 +0330

    add static and media configs and add requirements.txt

--- core/settings.py
+++ core/settings.py
@@ -118,6 +118,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / "static"
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static_files",
+]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
